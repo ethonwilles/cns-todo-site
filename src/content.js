13b94e86bcaabc64aa8836537ec2974d6da55cdd
ethonwilles <ethonwilles@gmail.com>
2020-06-04 13:02:37 -0600

    asdf

--- src/content.js
+++ src/content.js
@@ -4,7 +4,7 @@ import Todo from "./todo";
 const Content = () => {
   const [tasks, setTasks] = React.useState([]);
   React.useEffect(() => {
-    fetch("http://localhost:5000/todo-check")
+    fetch("https://cns-automate-backend.herokuapp.com/todo-check")
       .then((res) => res.json())
       .then((data) => setTasks(data.items));
   }, []);
